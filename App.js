824f65a8f44c3e2506a6fc314b7ec6e8b616acba
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2024-04-11 15:15:07 +0200

    Bottom Tabs navigation

--- App.js
+++ App.js
@@ -1180,36 +1180,82 @@ const logoImg2 = require("./assets/favicon.png")
 // *********** Drawer Navigator ***********//
 
 // import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
+// import { createDrawerNavigator } from '@react-navigation/drawer'
+// import DashboardScreen from './screens/Drawer/DashboardScreen'
+// import SettingsScreen from './screens/Drawer/SettingsScreen'
+
+// const Drawer = createDrawerNavigator()
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Drawer.Navigator>
+//         <Drawer.Screen
+//           name="Dashboard"
+//           component={DashboardScreen}
+//           options={{
+//             title: "My Dashboard", // Header title and Drawer label
+//             // If you want a different Drawer label
+//             drawerLabel: "Dashboard label",
+//             drawerActiveTintColor: "#333",
+//             drawerActiveBackgroundColor: "lightblue",
+//             drawerContentStyle: {
+//               backgroundColor: "#c6cbef",
+//             } 
+//           }}
+//         />
+//         <Drawer.Screen
+//           name="Settings"
+//           component={SettingsScreen}
+//         />
+
+//       </Drawer.Navigator>
+//     </NavigationContainer>
+//   )
+// }
+
+// *********** Bottom Tabs Navigator ***********//
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import DashboardScreen from './screens/Drawer/DashboardScreen'
 import SettingsScreen from './screens/Drawer/SettingsScreen'
+import CourseListScreen from './screens/Tabs/CourseListScreen'
+import ProfileScreen from './screens/Tabs/ProfileScreen'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
-const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen
-          name="Dashboard"
-          component={DashboardScreen}
-          options={{
-            title: "My Dashboard", // Header title and Drawer label
-            // If you want a different Drawer label
-            drawerLabel: "Dashboard label",
-            drawerActiveTintColor: "#333",
-            drawerActiveBackgroundColor: "lightblue",
-            drawerContentStyle: {
-              backgroundColor: "#c6cbef",
-            } 
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarLabelPosition: 'below-icon', // properties are beside-icon||below-icon, by default below-icon
+            tabBarShowLabel: true, // used to hide icons when set to false
+            tabBarActiveTintColor: "purple",
+            tabBarInactiveTintColor: "black",
+            tabBarActiveBackgroundColor: "black",
+            tabBarInactiveBackgroundColor: "purple",
           }}
-        />
-        <Drawer.Screen
-          name="Settings"
-          component={SettingsScreen}
-        />
-
-      </Drawer.Navigator>
-    </NavigationContainer>
-  )
-}
+        >
+          <Tab.Screen
+            name='Course List'
+            component={CourseListScreen}
+          />
+          <Tab.Screen
+            name='Profile'
+            component={ProfileScreen}
+            options={{
+              tabBarLabel: "My Profile",
+              tabBarIcon: ({color}) => <Ionicons name="person" size={20} color={color} />,
+              tabBarBadge: 7
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsScreen}
+          />
+  
+        </Tab.Navigator>
+      </NavigationContainer>
+    )
+  }
